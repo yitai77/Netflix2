c556bb5279e0549197dec15ddecc6ece857738b2
{yitai77} <{yitai7@naver.com}>
2023-06-28 00:11:53 +0900

    01

--- pages/03_03.py
+++ pages/03_03.py
@@ -9,14 +9,14 @@ Korean = common.get_ko_data()
 
 sk_year_counts = Korean['release_year'].value_counts().sort_index()
 
-color = ['violet']
+color = 'violet'
 
 fig, ax = plt.subplots()
 ax.bar(sk_year_counts.index, sk_year_counts.values, color=color)
 ax.set_xlabel('Year')
 ax.set_ylabel('Count')
 ax.set_xticks(sk_year_counts.index, sk_year_counts.index.astype(int), rotation=45)
-ax.set_title('Netflix Shows in the South Korea by year')
+ax.set_title('Netflix Shows in the South Korea by Year')
 st.pyplot(fig)
 
 
@@ -24,21 +24,23 @@ US = common.get_us_data()
 
 us_year_counts = US['release_year'].value_counts().sort_index()
 
-color = ['olive']
+color = 'olive'
 
 
-start_year = us_year_counts.index.min()  # 최소 연도
-end_year = us_year_counts.index.max()    # 최대 연도
-
-xticks = np.arange(start_year, end_year+1, 10)
 
 fig, ax = plt.subplots()
 ax.bar(us_year_counts.index, us_year_counts.values, color=color)
 ax.set_xlabel('Year')
 ax.set_ylabel('Count')
-# ax.set_xticklabels(xticks.astype(int), rotation=45)
-ax.set_xticklabels(xticks.astype(int), rotation=45)  # 올바른 메서드 이름 사용
-ax.set_title('Netflix Shows in the US by year')
+
+start_year = US['release_year'].min()
+end_year = (US['release_year'].max() // 10 * 10) + 10
+
+xticks = range(start_year, end_year, 10)
+ax.set_xticks(xticks)  # Set x-ticks at each decade
+ax.set_xticklabels([f'{i}s' for i in xticks], rotation=0)
+
+ax.set_title('Netflix Shows in the US by Year')
 st.pyplot(fig)
 
 
